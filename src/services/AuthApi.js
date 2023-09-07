47ff2386fbc04888fdd16401dcfdf1589b4e0498
joriskian <jo_risk@hotmail.com>
2023-09-07 10:39:17 +0200

    scripts start modified

--- src/services/AuthApi.js
+++ src/services/AuthApi.js
@@ -1,7 +1,7 @@
 export function hasAuthenticated() {
   // on va dire que ça a marché et qu'on est authentifié
-  console.log('now authenticated !!!');
-  return true;
+  // console.log('now authenticated !!!');
+  // return true;
   // on va dire que ça n'a pas marché et qu'on est pas authentifié
   console.log("oups, t'es pas authentifié");
   return false;
